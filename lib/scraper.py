33dd2f8f23cba3fef50c9997fe2b515d21639e3f
DaniAffCH <danieleaffinita2000@gmail.com>
2021-03-01 21:46:39 +0100

    organizzazione codice in librerie, proxy opzionale, conclusione I/O file

---
+++ lib/scraper.py
@@ -0,0 +1,69 @@
+from selectorlib import Extractor
+import requests
+import re
+
+class Scraper:
+    __private_url = str()
+    r = None
+    ext = None
+    headers = {
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'referer': 'https://www.amazon.com/',
+        'accept-language': 'it-IT;q=0.9,it;q=0.8',
+    }
+
+    ip_addresses = ["67.205.190.164:8080"]
+
+    def __init__(self, selector):
+        if selector == None or selector == "":
+            raise Exception("I parametri devono essere inizializzati")
+        self.ext = Extractor.from_yaml_file(selector)
+
+    def setUrl(self, u):
+        self.__private_url = u
+
+    def sendRequest(self, proxy = None):
+        if(self.__private_url == None or self.__private_url == ""):
+            raise Exception("Url non valido")
+        print("INVIO RICHIESTA")
+        if(proxy != None):
+            proxy_config = {"http": "http://"+proxy, "https": "http://"+proxy}
+            tmp = requests.get(self.__private_url, headers=self.headers, proxies=proxy_config)
+
+        else:
+            tmp = requests.get(self.__private_url, headers=self.headers)
+
+        if tmp.status_code > 400:
+            raise Exception("ERRORE! CODICE %s"%tmp.status_code)
+        elif "To discuss automated access to Amazon data please contact" in tmp.text:
+            raise Exception("AMAZON TI HA BLOCCATO\nURL CORRENTE %s"%self.__private_url)
+        else:
+            print("DOWNLOAD PAGINA COMPLETATO")
+            self.r = tmp
+
+    def __normalize(self, arr):
+        arr["price"] = arr["price"].replace(u'\xa0', u'')
+        pattern = r"n\. (\d+,*\d*) in (Alimentari e cura della casa|Oli d'oliva)"
+        arr["ranking"] = re.findall(pattern,arr["ranking"])
+
+        for n, element in enumerate(arr["ranking"]):
+            tmp = list(arr["ranking"][n])
+            tmp[0] = int(element[0].replace(',', ''))
+            arr["ranking"][n] = tuple(tmp)
+
+        pattern = r"\d+"
+        arr["reviews"] = int(re.search(pattern,arr["reviews"]).group())
+        return arr
+
+    def extraction(self):
+        if(self.r!=None):
+            out = self.ext.extract(self.r.text)
+            out["AmazonSpedition"] = "spedito da Amazon" in self.r.text
+            return self.__normalize(out)
--- main.py
+++ main.py
@@ -1,72 +1,5 @@
-from selectorlib import Extractor
-import requests
-import json
-import re
-import random
 from time import sleep
-
-class Scraper:
-    __private_url = str()
-    r = None
-    ext = None
-    headers = {
-        'dnt': '1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'referer': 'https://www.amazon.com/',
-        'accept-language': 'it-IT;q=0.9,it;q=0.8',
-    }
-
-    ip_addresses = ["181.129.183.19:53281"]
-
-    def __init__(self, selector):
-        if selector == None or selector == "":
-            raise Exception("I parametri devono essere inizializzati")
-        self.ext = Extractor.from_yaml_file(selector)
-
-    def setUrl(self, u):
-        self.__private_url = u
-
-    def sendRequest(self):
-        if(self.__private_url == None or self.__private_url == ""):
-            raise Exception("Url non valido")
-        print("INVIO RICHIESTA")
-        proxy = {"http": "http://"+self.ip_addresses[0], "https": "http://"+self.ip_addresses[0]}
-        tmp = requests.get(self.__private_url, headers=self.headers, proxies=proxy)
-
-        if tmp.status_code > 400:
-            raise Exception("ERRORE! CODICE %s"%tmp.status_code)
-        elif "To discuss automated access to Amazon data please contact" in tmp.text:
-            raise Exception("AMAZON TI HA BLOCCATO\nURL CORRENTE %s"%self.__private_url)
-        else:
-            print("DOWNLOAD PAGINA COMPLETATO")
-            self.r = tmp
-
-    def __normalize(self, arr):
-        arr["price"] = arr["price"].replace(u'\xa0', u'')
-        pattern = r"n\. (\d+,*\d*) in (Alimentari e cura della casa|Oli d'oliva)"
-        arr["ranking"] = re.findall(pattern,arr["ranking"])
-
-        for n, element in enumerate(arr["ranking"]):
-            tmp = list(arr["ranking"][n])
-            tmp[0] = int(element[0].replace(',', ''))
-            arr["ranking"][n] = tuple(tmp)
-
-        pattern = r"\d+"
-        arr["reviews"] = int(re.search(pattern,arr["reviews"]).group())
-        return arr
-
-    def extraction(self):
-        if(self.r!=None):
-            out = self.ext.extract(self.r.text)
-            out["AmazonSpedition"] = "spedito da Amazon" in self.r.text
-            return self.__normalize(out)
-
+from lib.scraper import Scraper
 
 def readFromFile(sc: Scraper, pathIn, pathOut = "info.txt"):
     with open(pathIn, 'r') as urlList:
@@ -77,13 +10,18 @@ def readFromFile(sc: Scraper, pathIn, pathOut = "info.txt"):
             print(res)
             with open(pathOut, 'a') as outputFile:
                 for element in res:
-                    outputFile.write(element+"\n")
+                    if type(res[element]).__name__ == 'list':
+                        outputFile.write(element+": ")
+                        for cat in res[element]:
+                            outputFile.write("posizione "+str(cat[0])+" nella categoria "+cat[1]+"\n")
+                    else:
+                        outputFile.write(element+": "+str(res[element])+"\n")
                 outputFile.write("\n\n")
 
 
 sel = "selettore.yml"
 sc = Scraper(sel)
-readFromFile(sc, "./list.txt")
+readFromFile(sc, "list.txt")
 
 # product_data = []
 # with open("search_results_urls.txt",'r') as urllist, open('search_results_output.jsonl','w') as outfile:
